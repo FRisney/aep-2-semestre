366e0881247a67ed02bb4e4ac24d2e33da040c1a
Felipe Ferreira <frisney@protonmail.com>
2020-11-08 11:13:09 -0300

    Desativa scroll ao abrir o menu lateral

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,9 +1,11 @@
 /* Open the sidenav */
 function openNav() {
     document.querySelector("nav").style.width = "100%";
+    document.querySelector("body").style.overflow = "hidden";
 }
 
 /* Close/hide the sidenav */
 function closeNav() {
     document.querySelector("nav").style.width = "0";
+    document.querySelector("body").style.overflow = "auto";
 }
